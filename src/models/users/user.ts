f7598cc1f830dc56dc6e9f627809ebeb6d590abd
iota <iota>
2021-03-19 11:39:52 +0100

    user

---
+++ src/models/users/user.ts
@@ -0,0 +1,11 @@
+class Users {
+  public sendMessages() {
+    console.log('Send message');
+  }
+  public getMessageUp() {
+    console.log('Get message up');
+  }
+  public getMessageDown() {
+    console.log('Get message down');
+  }
+}
